146e2b9afc11f5cfe58527903064ca622372a392
isuru <iwi@masterloop.com>
2019-02-11 14:34:27 +0530

    failed test

--- MySuper.Api/Controllers/ValuesController.cs
+++ MySuper.Api/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace MySuper.Api.Controllers
         public ActionResult<IEnumerable<string>> Get()
         {
            
-            return new string[] { "value_x", "value_z" };
+            return new string[] { "value_x_x", "value_z_z" };
         }
 
         // GET api/values/5
