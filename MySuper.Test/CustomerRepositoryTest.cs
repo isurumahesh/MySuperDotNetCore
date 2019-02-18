be5c85590ced712d5907b781c7c3f39cd81f5889
isuru <iwi@masterloop.com>
2019-02-18 15:11:52 +0530

    add unit tests

--- MySuper.Test/CustomerRepositoryTest.cs
+++ MySuper.Test/CustomerRepositoryTest.cs
@@ -23,8 +23,28 @@ namespace MySuper.Test
                 var customerRepository = new CutomerRepository(context);
                 var customer = new Customer { FirstName = "Isuru" };
                 customerRepository.InsertCustomer(customer);
-                     
-                Assert.Equal(1, customer.Id);
+
+                Assert.True(customer.Id > 0);
+            }
+        }
+
+        [Fact]
+        public void Should_Display_Customer_List_Count_As_Two()
+        {
+            optionsBuilder.UseInMemoryDatabase("MySuperTestDb");
+
+            // Run the test against one instance of the context
+            using (var context = new MySuperContext(optionsBuilder.Options))
+            {
+                var customerRepository = new CutomerRepository(context);
+                var customer1 = new Customer { FirstName = "Isuru" };
+                var customer2 = new Customer { FirstName = "Mahesh" };
+
+                customerRepository.InsertCustomer(customer1);
+                customerRepository.InsertCustomer(customer2);
+
+                var count = context.Customers.Count();
+                Assert.Equal(3, count);
             }
         }
     }
